ff9a568d169acc96027f32e3f5e7df32a86683df
Abu-Sufian Shibli <62396798+abusufianshibli@users.noreply.github.com>
2020-07-25 22:18:29 +0600

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# flutter2
+# donateplasma
+
+[![Actions Status](https://github.com/teamtigers/donate_plasma/workflows/flutter-driver/badge.svg)](https://github.com/TeamTigers/donate_plasma/runs/816041523?check_suite_focus=true)
 
 A new Flutter application.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,180 +1,46 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter2/animation/FadeAnimation.dart';
+import 'package:donateplasma/models/User.dart';
+import 'package:donateplasma/providers/information.dart';
+import 'package:donateplasma/providers/users.dart';
+import 'package:donateplasma/screens/BannerScreen.dart';
+import 'package:donateplasma/screens/HomeScreen.dart';
+import 'package:donateplasma/screens/edit_profile.dart';
+import 'package:donateplasma/screens/story_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter2/story.dart';
+import 'package:donateplasma/constants/color_constants.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    )
-);
+void main() {
+  runApp(MyApp());
+}
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFFFFBF05),
-        title:Center(
-          child: Container(
-            child: Text("Edit Profile", style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),),
-            decoration: BoxDecoration(
-                border: Border(bottom: BorderSide(color:Color(0xFFFFBF05).withOpacity(.8)))
-            ),
-          ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Information(),
         ),
-
-
-      ),
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Container(
-            child: Column(
-
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.only(top:20,),
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/dn.png'),
-                          fit: BoxFit.fill
-                      )
-                  ),
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned(
-                        child: FadeAnimation(1.6, Container(
-                          margin: EdgeInsets.only(bottom: 270,right: 200),
-                          child: Center(
-                            child:FlatButton(
-                              child: Text('Profile', style: TextStyle(color: Color(0xFFFFBF05),fontSize: 20,fontStyle: FontStyle.italic),),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.red)
-                                ),
-                              onPressed: () {},
-                            ),
-                          ),
-                        )),
-                      ),
-                      Positioned(
-                        child: FadeAnimation(1.6, Container(
-                          margin: EdgeInsets.only(bottom: 270,left: 200),
-                          child: Center(
-                            child:FlatButton(
-                              child: Text('Story', style: TextStyle(color: Color(0xFFFFBF05),fontSize: 20,fontStyle: FontStyle.italic,
-                              ),
-                              ),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.red)
-                                ),
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => story()),
-                                );
-                              },
-                            ),
-                          ),
-                        )),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top:.2,left: 35,right: 35),
-                  child: Column(
-                    children: <Widget>[
-                      FadeAnimation(1.8, Container(
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          boxShadow: <BoxShadow>[
-                            BoxShadow(
-                              offset: Offset(-4, 4),
-                              blurRadius: 10,
-                              color: Color(0xFFFFBF05).withOpacity(.6),
-                            )
-                          ],
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              padding: EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color:Color(0xFFFFBF05).withOpacity(.10)))
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Name",
-                                    hintStyle: TextStyle(color: Colors.grey[400])
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color:Color(0xFFFFBF05).withOpacity(.10)))
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Blood Group",
-                                    hintStyle: TextStyle(color: Colors.grey[400])
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Color(0xFFFFBF05).withOpacity(.10)))
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Phone number",
-                                    hintStyle: TextStyle(color: Colors.grey[400])
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(8.0),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Location",
-                                    hintStyle: TextStyle(color: Colors.grey[400])
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      )),
-                      SizedBox(height: 30,),
-                      FadeAnimation(2, Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                           color: Color(0xfffbbd5c),
-
-
-                        ),
-                        child: Center(
-                          child: Text("Save", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold ,fontSize: 20),),
-                        ),
-                      )),
-                      SizedBox(height: 70,),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
+        ChangeNotifierProvider(
+          create: (ctx) => Users(),
         )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: kPrimaryColor,
+          accentColor: kAccentColor
+        ),
+        home: BannerScreen(),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          HomeScreen.routeName: (ctx) => HomeScreen(),
+          BannerScreen.routeName: (ctx) => BannerScreen(),
+          EditProfile.routeName: (ctx) => EditProfile(),
+          StoryScreen.routeName: (ctx) => StoryScreen()
+        },
+      ),
     );
   }
 }
